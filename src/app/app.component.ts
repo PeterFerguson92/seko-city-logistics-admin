653a93d85c4bddf80ddd3ae7eef40f2105035b50
jrferguson <1q2w3e4r5t6y>
2022-02-18 22:26:43 +0000

    login and sidevnav quick refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { NavigationEnd, Router, RoutesRecognized } from '@angular/router';
+import { BreakpointObserver} from '@angular/cdk/layout'
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild(MatSidenav)
+  sidenav! : MatSidenav;
   isVisible = true;
   LOGIN_URL = '/login'
 
-  constructor(route: Router) {
+  constructor(route: Router, private observer : BreakpointObserver) {
     route.events.subscribe(event => {
       if (event instanceof RoutesRecognized) {
         this.isVisible = event.url !== this.LOGIN_URL;
@@ -20,6 +24,19 @@ export class AppComponent {
         this.isVisible = event.url !== this.LOGIN_URL;
       }
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res)=>{
+      if(res.matches){
+        this.sidenav.mode ='over';
+        this.sidenav.close()
+      }
+      else{
+        this.sidenav.mode = 'side';
+        this.sidenav.open()
+      }
+    })
   }
 
 }
