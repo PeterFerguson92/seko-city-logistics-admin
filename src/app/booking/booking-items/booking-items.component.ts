fff81391e0051225c174caeab16a6d79f95c6b69
jrferguson <1q2w3e4r5t6y>
2022-03-14 16:00:26 +0000

    added multi items

--- src/app/booking/booking-items/booking-items.component.ts
+++ src/app/booking/booking-items/booking-items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { BOOKING_ITEMS, BOOKING_ITEMS_DISPLAY_NAMES } from 'src/app/constants';
 
 @Component({
@@ -9,30 +9,65 @@ import { BOOKING_ITEMS, BOOKING_ITEMS_DISPLAY_NAMES } from 'src/app/constants';
 })
 export class BookingItemsComponent implements OnInit {
   bookingItemForm: FormGroup;
-  types = BOOKING_ITEMS_DISPLAY_NAMES
+  showDescription = false;
+  types = BOOKING_ITEMS_DISPLAY_NAMES;
+
+  get items(): FormArray {
+    return this.bookingItemForm.get('items') as FormArray;
+  }
+
 
   constructor(private formBuilder: FormBuilder) {
     this.bookingItemForm = this.formBuilder.group({
-      quantity: ['', [Validators.required]],
-      type: [this.types[0], Validators.required],
-      description: ['', Validators.required],
-      value: ['', Validators.required],
-      pricePerUnit: ['', Validators.required],
-      totalPrice: ['', Validators.required]
+      items: this.formBuilder.array([this.buildItem()])
     });
   }
 
   ngOnInit(): void {
   }
 
-  onSelectionChange(event: any, fControlName: string) {
-    const fControl = this.getFormControl(fControlName);
+  onSelectionChange(event: any, fControlName: string, index) {
+    const fControl = this.getFormControl(fControlName, index);
+    const descriptionFormControl = this.getFormControl('description', index);
+    if (fControlName === 'type' && event.value === 'OTHER')
+    {
+      this.showDescription = true;
+      descriptionFormControl.setValidators([Validators.required]);
+      // this.validateFormControl('otherDestination');
+    } else
+    {
+      this.showDescription = false;
+      descriptionFormControl.setValidators([]);
+      descriptionFormControl.markAsPristine();
+      descriptionFormControl.setValue('');
+    }
     fControl.setValue(event.value);
     fControl.markAsDirty();
   }
 
-  getFormControl(fControlName: string) {
-    return this.bookingItemForm.get(fControlName)
+  getFormControl(fControlName: string, index) {
+    return this.items.controls[index].get(fControlName)
   }
 
+  buildItem(): FormGroup {
+    return this.formBuilder.group({
+      quantity: [0, [Validators.required]],
+      types: [this.types[0], Validators.required],
+      description: ['', Validators.required],
+      value: ['', Validators.required],
+      pricePerUnit: ['0', Validators.required],
+      totalPrice: ['0', Validators.required]
+    })
+  }
+
+  onAddItem() {
+    this.items.push(this.buildItem());
+  }
+
+  onDeleteItem(index) {
+    if (this.items.length > 1)
+    {
+      this.items.removeAt(index)
+    }
+  }
 }
--- src/app/shared/inputs/material-numeric-input/material-numeric-input.component.ts
+++ src/app/shared/inputs/material-numeric-input/material-numeric-input.component.ts
@@ -1,16 +1,68 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit, Optional, ViewChild } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DEFAULT_INPUT_COLOR, ALERT_INPUT_COLOR } from 'src/app/constants';
 
 @Component({
   selector: 'app-material-numeric-input',
   templateUrl: './material-numeric-input.component.html',
   styleUrls: ['./material-numeric-input.component.css']
 })
-export class MaterialNumericInputComponent implements OnInit {
-  @Input() label;
+export class MaterialNumericInputComponent implements OnInit, ControlValueAccessor {
+  @ViewChild('input', { static: true }) public input: MatInput;
 
-  constructor() { }
+  @Input() label;
+  @Input() icon;
+  @Input() width;
+  @Input() errorMsg: string;
+  @Input() type;
+  value;
+
+  inputColor;
+  disabled;
+
+  @HostBinding('attr.style')
+  public get valueAsStyle(): any {
+    return this.sanitizer.bypassSecurityTrustStyle(`--input-color: ${this.inputColor}`);
+  }
+  constructor(@Optional() public controlDir: NgControl, private sanitizer: DomSanitizer) {
+    this.inputColor = DEFAULT_INPUT_COLOR
+    if (controlDir) {
+      controlDir.valueAccessor = this;
+    }
+  }
 
   ngOnInit(): void {
+    if (this.controlDir)
+    {
+      const control = this.controlDir.control;
+      control.updateValueAndValidity();
+    }
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj
+    this.input.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+  onChange(event) { }
+
+  onTouched() {}
+
+
+  getDisplayLabel() {
+   this.inputColor = this.controlDir.control.dirty && this.controlDir.invalid ? ALERT_INPUT_COLOR : DEFAULT_INPUT_COLOR
+  return  this.label;
   }
 
 }
