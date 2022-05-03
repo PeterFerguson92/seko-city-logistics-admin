cd1a5e5bb9eb6f63f857ab1b20f001b103bc5010
jrferguson <1q2w3e4r5t6y>
2022-05-03 23:43:43 +0100

    small tidy up

---
+++ src/app/shipment/service/resolver/shipment.resolver.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ShipmentResolver} from './shipment.resolver';
+
+describe('ShipmentResolverResolver', () => {
+  let resolver: ShipmentResolver;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    resolver = TestBed.inject(ShipmentResolver);
+  });
+
+  it('should be created', () => {
+    expect(resolver).toBeTruthy();
+  });
+});
