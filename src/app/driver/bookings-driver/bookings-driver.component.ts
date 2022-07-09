c3f26a9e29181fb627f6f8fc205745f737755467
jrferguson <1q2w3e4r5t6y>
2022-07-09 15:53:14 +0100

    added bookings summary

--- src/app/driver/bookings-driver/bookings-driver.component.ts
+++ src/app/driver/bookings-driver/bookings-driver.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CommonService } from 'src/app/service/common.service';
 
 @Component({
@@ -15,14 +15,17 @@ export class BookingsDriverComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   dataSource = null;
-  displayedColumns: string[] = ['ID', 'SENDER', 'POSTCODE', 'ADDRESS', 'DATE', 'TIME', 'PHONE', 'STATUS'];
-  constructor(private activatedroute: ActivatedRoute, private commonService: CommonService,) { }
+  displayedColumns: string[] = ['ID', 'SENDER', 'POSTCODE', 'ADDRESS', 'DATE', 'TIME', 'PHONE', 'STATUS', 'ACTION'];
+  constructor(private activatedroute: ActivatedRoute, private commonService: CommonService, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedroute.data.subscribe(data => {
       this.dataSource = new MatTableDataSource(data.bookings);
-      console.log(data);
     });
+  }
+
+  onViewBooking(reference) {
+    this.router.navigate(['/booking-summary', reference]);
   }
 
   getFormattedDate(date) {
