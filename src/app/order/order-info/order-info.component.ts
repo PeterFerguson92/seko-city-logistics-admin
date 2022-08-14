cdf2a6057225659dcbb0e8d8a59c2e35ee233796
jrferguson <1q2w3e4r5t6y>
2022-08-14 01:39:23 +0100

    Order info Ui

--- src/app/order/order-info/order-info.component.ts
+++ src/app/order/order-info/order-info.component.ts
@@ -1,7 +1,10 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
-import { ORDER_PICKUP_TIMES, PAYMENT_STATUSES, PAYMENT_TYPES } from 'src/app/constants';
+import {
+  FULL_PAYMENT_STATUS_ALIAS, NO_PAYMENT_PAYMENT_STATUS_ALIAS,
+  ORDER_ITEMS, ORDER_ITEMS_TYPES_DISPLAY_NAMES, ORDER_PICKUP_TIMES, PARTIAL_PAYMENT_STATUS_ALIAS, PAYMENT_STATUSES, PAYMENT_TYPES
+} from 'src/app/constants';
 import { CommonService } from 'src/app/service/common.service';
 import { ValidationService } from 'src/app/service/validation/validation.service';
 
@@ -19,6 +22,12 @@ export class OrderInfoComponent implements OnInit, AfterViewInit {
   formValidationMap = { postcode: '' };
   paymentTypes = PAYMENT_TYPES
   paymentStatuses = PAYMENT_STATUSES;
+  types = ORDER_ITEMS_TYPES_DISPLAY_NAMES;
+
+
+  get items(): FormArray {
+    return this.orderInfoForm.get('items') as FormArray;
+  }
 
   constructor(private formBuilder: FormBuilder, private dialog: MatDialog,private commonService: CommonService,
     private validationService: ValidationService) { }
@@ -35,10 +44,66 @@ export class OrderInfoComponent implements OnInit, AfterViewInit {
       paymentType: [this.orderInfo.paymentType ? this.orderInfo.paymentType : this.paymentTypes[0] , [Validators.required]],
       paymentStatus: [this.orderInfo.paymentStatus ? this.orderInfo.paymentStatus : this.paymentStatuses[0], Validators.required],
       paymentNotes: [this.orderInfo.paymentNotes ? this.orderInfo.paymentNotes : '', []],
-      amountPaid: [this.orderInfo.amountPaid ? this.orderInfo.amountPaid : 0,  []],
-      amountOutstanding: [this.orderInfo.amountOutstanding ? this.orderInfo.amountOutstanding: 0 , []]
-
+      totalAmount: [this.orderInfo.totalAmount ? this.orderInfo.totalAmount : 0,  []],
+      amountPaid: [this.orderInfo.amountPaid ? this.orderInfo.amountPaid : 0, []],
+      amountOutstanding: [this.orderInfo.amountOutstanding ? this.orderInfo.amountOutstanding : 0, []],
+      items: this.formBuilder.array([this.buildItemGroup(null)])
     })
+    this.getFormControl('totalAmount').disable();
+    this.getFormControl('amountPaid').disable();
+    this.getFormControl('amountOutstanding').disable();
+    this.initializeProperties(0);
+    this.updateTotalAmount()
+
+  }
+
+  buildItemGroup(item): FormGroup {
+    return this.formBuilder.group({
+      type: [this.types[0], [Validators.required]],
+      pricePerUnit: [this.getPricePerUnit(this.types[0])],
+      quantity: [1],
+      amount: [0]
+    })
+  }
+
+  initializeProperties(index) {
+    this.getItemsFormControl('pricePerUnit', index).disable()
+    this.getItemsFormControl('amount', index).disable();
+    this.updateItemTotalAmount(index)
+  }
+
+  setPaymentProperties(paymentStatus: string) {
+    const totalAmountControl =  this.getFormControl('totalAmount');
+    const amountPaidControl =  this.getFormControl('amountPaid');
+    const outstandingPaidControl =  this.getFormControl('amountOutstanding');
+
+    switch(paymentStatus) {
+      case FULL_PAYMENT_STATUS_ALIAS:
+        amountPaidControl.setValue(totalAmountControl.value);
+        outstandingPaidControl.setValue(0);
+        amountPaidControl.disable();
+        break;
+      case PARTIAL_PAYMENT_STATUS_ALIAS:
+        outstandingPaidControl.setValue(outstandingPaidControl.value)
+        amountPaidControl.enable();
+        break;
+      case NO_PAYMENT_PAYMENT_STATUS_ALIAS:
+        amountPaidControl.setValue(0);
+        amountPaidControl.disable();
+        break;
+    }
+
+  }
+
+  updateItemTotalAmount(index) {
+    const itemPricePerUnit = this.getItemsFormControl('pricePerUnit', index).value;
+    const itemQuantity = this.getItemsFormControl('quantity', index).value;
+
+    this.getItemsFormControl('amount', index).setValue(itemPricePerUnit * itemQuantity);
+    this.getItemsFormControl('amount', index).markAsDirty();
+    this.updateTotalAmount();
+    this.updateOutstandingAmount()
+
   }
 
   ngAfterViewInit(): void {
@@ -65,9 +130,43 @@ export class OrderInfoComponent implements OnInit, AfterViewInit {
 
   onSelectionChange(event: any, fControlName: string) {
     const fControl = this.getFormControl(fControlName);
-    const data = fControlName === 'updatesViaWhatsapp' ? event.checked : event.value;
+    const data = fControlName === 'updatesViaWhatsapp' ||  fControlName === 'updatesViaEmail'? event.checked : event.value;
     fControl.setValue(data);
     fControl.markAsDirty();
+    if (fControlName === 'paymentStatus')
+    {
+      this.setPaymentProperties(event.value)
+    }
+  }
+
+  onItemsSelectionChange(event: any, fControlName: string, index: number) {
+    const fControl = this.getItemsFormControl(fControlName, index);
+    fControl.setValue(event.value);
+    fControl.markAsDirty();
+    this.getItemsFormControl('pricePerUnit', index).setValue(this.getPricePerUnit(fControl.value));
+    this.updateItemTotalAmount(index)
+    this.updateTotalAmount();
+  }
+
+  onAddItem() {
+    this.items.push(this.buildItemGroup(null));
+    this.initializeProperties(this.items.length - 1);
+    this.updateTotalAmount();
+    this.updateOutstandingAmount()
+  }
+
+  onDeleteItem(index) {
+    if (this.items.length > 1)
+    {
+      this.items.removeAt(index)
+      this.updateTotalAmount();
+      this.updateOutstandingAmount()
+    }
+  }
+
+  getItemsFormControl(fControlName: string, index: number) {
+    const fControl = this.items.controls[index];
+    return fControl.get(fControlName)
   }
 
   getAddressByPostcode() {
@@ -77,6 +176,18 @@ export class OrderInfoComponent implements OnInit, AfterViewInit {
       this.addresses = this.commonService.getAddressesByPostcode(postcodeFormControl.value);
       return this.addresses;
     }
+  }
+
+  getPricePerUnit(selection: string) {
+    let price = null;
+    ORDER_ITEMS.forEach((element) => {
+      if (element.DISPLAY_NAME === selection)
+      {
+        price = parseInt(element.PRICE, 10);
+      }
+    });
+
+    return price;
   }
 
   getInfoDetails() {
@@ -94,18 +205,41 @@ export class OrderInfoComponent implements OnInit, AfterViewInit {
     return info;
   }
 
-  updateOutstandingAmount() { // toDO  vedere se questo calcolo si puo fare meglio senza dover chiamare items tutte le volte
-    // this.isAmountPaidEnabled()
-    // const totalAmount = this.getItemsDetails().totalAmount;
-    // let amountPaid = this.paymentForm.get('amountPaid').value;
+  isDeleteDisabled() {
+    return this.items.length === 1;
+  }
 
-    // if (amountPaid >= totalAmount)
-    // {
-    //   amountPaid = totalAmount;
-    //   this.paymentForm.get('amountPaid').setValue(amountPaid);
-    //   this.paymentForm.get('paymentStatus').setValue(FULL_PAYMENT_STATUS_ALIAS)
-    // }
-    // this.paymentForm.get('amountOutstanding').setValue(totalAmount - amountPaid);
+  updateTotalAmount() {
+    let totalAmount = 0
+    this.items.controls.forEach((control) => {
+      totalAmount = totalAmount + control.get('amount').value;
+    });
+
+    this.getFormControl('totalAmount').setValue(totalAmount);
+  }
+
+  updateOutstandingAmount() {
+    const totalAmountControl = this.getFormControl('totalAmount');
+    const amountPaidControl = this.getFormControl('amountPaid');
+    const outstandingPaidControl = this.getFormControl('amountOutstanding');
+    if (parseInt(amountPaidControl.value, 10) <= 0)
+    {
+      amountPaidControl.setValue(0);
+      outstandingPaidControl.setValue(totalAmountControl.value);
+      this.getFormControl('paymentStatus').setValue(NO_PAYMENT_PAYMENT_STATUS_ALIAS)
+    } else
+    {
+      if (amountPaidControl.value >= totalAmountControl.value)
+      {
+        amountPaidControl.setValue(totalAmountControl.value);
+        this.getFormControl('paymentStatus').setValue(FULL_PAYMENT_STATUS_ALIAS)
+        outstandingPaidControl.setValue(0);
+      } else
+      {
+        this.getFormControl('paymentStatus').setValue(PARTIAL_PAYMENT_STATUS_ALIAS)
+        outstandingPaidControl.setValue(totalAmountControl.value - amountPaidControl.value);
+      }
+    }
   }
 
 }
