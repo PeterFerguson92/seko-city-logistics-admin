b35d09f4a339fe77e5fc345b2eb59caa2ab1b1ad
jrferguson <1q2w3e4r5t6y>
2022-09-24 00:09:13 +0100

    invoice dialog

--- src/app/booking/attach-invoice-dialog/attach-invoice-dialog.component.ts
+++ src/app/booking/attach-invoice-dialog/attach-invoice-dialog.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-attach-invoice-dialog',
   templateUrl: './attach-invoice-dialog.component.html',
-  styleUrls: ['./attach-invoice-dialog.component.css']
+  styleUrls: ['./attach-invoice-dialog.component.css', '../../shared/shared.dialog.css']
 })
 export class AttachInvoiceDialogComponent implements OnInit {
 
