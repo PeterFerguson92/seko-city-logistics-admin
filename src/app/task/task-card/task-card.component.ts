37fc5ccd8fbe43c6c6c270bbd3915b8c68dd8eb0
jrferguson <1q2w3e4r5t6y>
2024-05-29 16:38:42 +0100

    task creation date

--- src/app/task/task-card/task-card.component.ts
+++ src/app/task/task-card/task-card.component.ts
@@ -7,83 +7,85 @@ import { AddEditTaskDialogComponent } from '../add-edit-task-dialog/add-edit-tas
 import { TaskService } from '../service/task.service';
 
 @Component({
-  selector: 'app-task-card',
-  templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.css']
+    selector: 'app-task-card',
+    templateUrl: './task-card.component.html',
+    styleUrls: ['./task-card.component.css'],
 })
 export class TaskCardComponent implements OnInit {
-  @Input() task;
+    @Input() task;
 
-  constructor(private commonService: CommonService,
-    private taskService: TaskService,
-    private dialog: MatDialog) { }
+    constructor(
+        private commonService: CommonService,
+        private taskService: TaskService,
+        private dialog: MatDialog
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
-  onViewTask() {
-    const dialogRef = this.dialog.open(AddEditTaskDialogComponent, {
-      height: '65%',
-      width: '50%',
-      data: { task: this.task }
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(result)
-    })
-  }
-
-  OnDeleteTask(id) {
-    const dialogRef = this.dialog.open(ConfirmDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === 'true')
-      {
-        this.taskService.deleteTask(id).subscribe(
-          ({ data }) => {
-            location.reload()
-          },
-          error => {
-            console.log(error);
-          }
-        )
-      }
-    })
-  }
-
-  getFormattedDate(date) {
-    return this.commonService.getFormattedDate(date)
-  }
-
-  isWebPickUp(task) {
-    return task.title === WEB_TASK_PICK_UP_REQUEST || task.title === WEB_TASK_ORDER_REQUEST;
-  }
-
-  getDisplayText(task) {
-    if (this.isWebPickUp(task))
-    {
-      const info = task.description.split('-');
-      return info
-    } else
-    {
-      return task.description;
-    }
-  }
-
-  getTaskClass(task) {
-    const today = new Date(new Date()).toISOString();
-    const todayNewDate = new Date(this.commonService.getFormattedDate(today).split('/').reverse().join('/'));
-    const taskDueDate = new Date(this.commonService.getFormattedDate(task.actionDate).split('/').reverse().join('/'));
-
-    if (todayNewDate  >
-      taskDueDate && task.status !== 'DONE')
-    {
-      return { pastDueDate:true }
+    onViewTask() {
+        const dialogRef = this.dialog.open(AddEditTaskDialogComponent, {
+            height: '65%',
+            width: '50%',
+            data: { task: this.task },
+        });
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log(result);
+        });
     }
 
-    if (this.commonService.getFormattedDate(today) ===
-      this.commonService.getFormattedDate(task.actionDate) && task.status !== 'DONE')
-    {
-      return { currentDueDate:true }
+    OnDeleteTask(id) {
+        const dialogRef = this.dialog.open(ConfirmDialogComponent);
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result === 'true') {
+                this.taskService.deleteTask(id).subscribe(
+                    ({ data }) => {
+                        location.reload();
+                    },
+                    (error) => {
+                        console.log(error);
+                    }
+                );
+            }
+        });
     }
-  }
 
+    getFormattedDate(date) {
+        return this.commonService.getFormattedDate(date);
+    }
+
+    getCreationDateFormatted(creationDate) {
+        return new Date(creationDate * 1000).toUTCString();
+    }
+
+    isWebPickUp(task) {
+        return task.title === WEB_TASK_PICK_UP_REQUEST || task.title === WEB_TASK_ORDER_REQUEST;
+    }
+
+    getDisplayText(task) {
+        if (this.isWebPickUp(task)) {
+            const info = task.description.split('-');
+            return info;
+        } else {
+            return task.description;
+        }
+    }
+
+    getTaskClass(task) {
+        const today = new Date(new Date()).toISOString();
+        const todayNewDate = new Date(this.commonService.getFormattedDate(today).split('/').reverse().join('/'));
+        const taskDueDate = new Date(
+            this.commonService.getFormattedDate(task.actionDate).split('/').reverse().join('/')
+        );
+
+        if (todayNewDate > taskDueDate && task.status !== 'DONE') {
+            return { pastDueDate: true };
+        }
+
+        if (
+            this.commonService.getFormattedDate(today) === this.commonService.getFormattedDate(task.actionDate) &&
+            task.status !== 'DONE'
+        ) {
+            return { currentDueDate: true };
+        }
+    }
 }
